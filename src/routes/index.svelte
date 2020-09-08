33db28247591ac454a928d4ded4b86195b803d9d
Olawale Oyeyipo <olaclassik@gmail.com>
2020-09-08 12:24:32 +0100

    remove styling from landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,5 @@
 <style>
-  h1 {
-    text-align: center;
-    margin: 0 auto;
-  }
 
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
 </style>
 
 <svelte:head>
