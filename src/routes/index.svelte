1291dabff88fed766114d8e6c287c73eba0af184
Olawale Oyeyipo <olaclassik@gmail.com>
2020-09-08 12:16:16 +0100

    minor landing page clean up

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,5 @@
 <style>
-  h1,
-  figure,
-  p {
+  h1 {
     text-align: center;
     margin: 0 auto;
   }
@@ -12,35 +10,10 @@
     font-weight: 700;
     margin: 0 0 0.5em 0;
   }
-
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
-  }
 </style>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>Oyeyipo Olawale | Software Developer</title>
 </svelte:head>
 
-<h1>Git integration works for olawale oyeyipo!</h1>
-
-<figure>
-  <img alt="Success Kid" src="mypic.jpg" />
-  <figcaption>Have fun with Sapper!</figcaption>
-</figure>
+<h1>Hello, I am Olawale Oyeyipo</h1>
