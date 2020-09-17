48f9bab82f067d437461e44a71901d07ad5d3fd7
Olawale Oyeyipo <olaclassik@gmail.com>
2020-09-17 06:55:36 +0100

    add core general app

--- backend/settings.py
+++ backend/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+# ======================
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APP = []
+
+LOCAL_APP = [
+    "backend.blog",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APP + LOCAL_APP
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
