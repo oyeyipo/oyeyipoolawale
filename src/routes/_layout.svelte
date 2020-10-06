3a5351096317d4fc6ca07b0d4b09e85b0c0e5707
Olawale Oyeyipo <olaclassik@gmail.com>
2020-10-06 14:00:55 +0100

    Styling workflow done

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,6 @@
 <script>
-  import Nav from "../components/Nav.svelte";
-  import Title from "../components/Title.svelte";
+  //   import Nav from "../components/Nav.svelte";
+  //   import Title from "../components/Title.svelte";
 
   //   export let segment;
 </script>
@@ -9,6 +9,8 @@
   @import "../styles/global.scss";
 
   main {
+    width: 100%;
+    height: 100vh;
     display: grid;
     place-items: center;
   }
