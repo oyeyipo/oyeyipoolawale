c80051e9b97f8c7b04fa1dcfe1f6907d394fbf30
Olawale Oyeyipo <olaclassik@gmail.com>
2020-10-06 14:51:58 +0100

    clean up jumbotron

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -7,13 +7,6 @@
 
 <style lang="scss" global>
   @import "../styles/global.scss";
-
-  main {
-    width: 100%;
-    height: 100vh;
-    display: grid;
-    place-items: center;
-  }
 </style>
 
 <!-- <Nav {segment} /> -->
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,36 @@
 <script>
   import Title from "../components/Title.svelte";
+  import Social from "../components/Social.svelte";
 </script>
 
 <style lang="scss">
   h1 {
-    color: var(--col);
-    background-color: var(--col2);
+    line-height: 1;
+
+    > span {
+      display: block;
+    }
+
+    .first {
+      font-size: var(--ss-rem-7);
+      color: var(--color-primary-300);
+    }
+    .main {
+      font-size: var(--ss-rem-8);
+    }
+  }
+  p {
+    text-transform: capitalize;
+    font-size: var(--ss-rem-5);
+  }
+
+  .jumbotron {
+    background-color: var(--color-primary-100);
+    padding: var(--ss-rem-6);
+    width: 100%;
+
+    display: grid;
+    row-gap: var(--ss-rem-4);
   }
 </style>
 
@@ -13,7 +38,12 @@
   <Title>Home</Title>
 </svelte:head>
 
-<div>
-  <h1>Hey, I'm Olawale Oyeyipo 👨‍💻_</h1>
+<div class="jumbotron">
+  <h1>
+    <span class="first">Hi, </span>
+    <span class="main">I'm Olawale Oyeyipo</span>
+  </h1>
   <p>Software Developer/Writer who loves to solve problems and teach.</p>
+
+  <Social />
 </div>
