c1df9697b4c62bf98c48898041091346966d4b98
Olawale Oyeyipo <olaclassik@gmail.com>
2020-10-06 13:53:55 +0100

    create backend

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from backend.core.models import TimestampedModel
+
+class Post(TimestampedModel):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
