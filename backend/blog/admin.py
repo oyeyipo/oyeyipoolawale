89e27969d81d8090b559c8e01304aa97c8f13406
Olawale Oyeyipo <olaclassik@gmail.com>
2020-10-11 15:35:41 +0100

    Added slug field and updated admin form

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    exclude = ("slug",)
+    list_display = ("title", "updated_at", "created_at")
+    list_display_link = "updated_at"
+    list_filter = ("updated_at", "created_at")
+    search_fields = ("title", "updated_at")
--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,10 +1,33 @@
 from django.db import models
+from django.utils.text import slugify
 
 from backend.core.models import TimestampedModel
 
+
 class Post(TimestampedModel):
     title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
     content = models.TextField()
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        print("In save method")
+        self.slug = create_slug(self)
+        super().save(*args, **kwargs)
+
+
+def create_slug(instance, new_slug=None):
+    print("In create_slug")
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    queryset = Post.objects.filter(slug=slug).order_by("-id")
+    exists = queryset.exists()
+    if exists:
+        new_slug = f"{slug}-{queryset.first().id}"
+        print("In create_slug exists")
+        return create_slug(instance, new_slug=new_slug)
+    print("In create_slug new")
+    return slug
