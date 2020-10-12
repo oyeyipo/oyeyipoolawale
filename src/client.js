e625d1ff69e84d8d2d395b802fc07e0c5c6a2e9f
Olawale Oyeyipo <olaclassik@gmail.com>
2020-10-12 13:32:56 +0100

    Clean up

--- src/client.js
+++ src/client.js
@@ -1,9 +1,5 @@
 import * as sapper from "@sapper/app";
 
-sapper
-  .start({
-    target: document.body,
-  })
-  .then(() => {
-    console.log("olawale svelte/sapper app has started");
-  });
+sapper.start({
+  target: document.body,
+});
