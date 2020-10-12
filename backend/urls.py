f20ccfcb8fd709557754ce7de5fff29c2d6917b6
Olawale Oyeyipo <olaclassik@gmail.com>
2020-10-12 05:28:54 +0100

    Adjust static file url

--- backend/urls.py
+++ backend/urls.py
@@ -2,12 +2,16 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="home"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
 
 
 urlpatterns += [
