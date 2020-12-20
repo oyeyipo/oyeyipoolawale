dc466b3fc3e9138cef560958302b2847b48bf187
Olawale Oyeyipo <olaclassik@gmail.com>
2020-12-20 14:32:35 +0100

    Fix heroku static fail to build
    fix static
    
    fix static 1
    
    fix static 2
    
    fix static 3
    
    fix 5
    
    cause trouble

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -130,9 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
