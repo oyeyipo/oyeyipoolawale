bf5d0fe37a992dc82856c3c9e702db5dd7eb008c
Olawale Oyeyipo <olaclassik@gmail.com>
2020-12-22 16:35:12 +0100

    cleanup: complete

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -12,17 +12,6 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Welcome to olawale's blog!!!</h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
