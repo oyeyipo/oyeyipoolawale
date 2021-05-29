760c3edf024cc380ab179d6e6dc2878058324bbd
Olawale Oyeyipo <olaclassik@gmail.com>
2021-05-29 15:42:39 +0100

    Restructure frontend app

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "public")
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
