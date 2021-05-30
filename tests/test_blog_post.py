f054bff2afbb4299cd8c9e4cabb47ff2b0db9e62
Olawale Oyeyipo <olaclassik@gmail.com>
2021-05-30 01:00:11 +0100

    FT user story for homepage blog posts

--- tests/test_blog_post.py
+++ tests/test_blog_post.py
@@ -3,8 +3,16 @@ from .base import FunctionalTest
 
 class ReadersTest(FunctionalTest):
     def test_can_see_list_of_blog_post_and_read_in_detail(self):
+        # Bola decide to check the personal website of Oyeyipo Olawale
         self.browser.get(self.live_server_url)
 
+        # On loading the website he notices that there is
+        # "Oyeyipo" both the title and header on the homepage.
         self.assertIn("Oyeyipo", self.browser.title)
         header_text = self.browser.find_element_by_tag_name("h1").text
         self.assertIn("Oyeyipo", header_text)
+
+        # He notices that the was a list of blog posts titles on the homepage too
+        # under the title "Blog Posts" that Oyeyipo has recently written.
+        # Bola check out one of the post and read it in detail.
+        self.fail('Finish me!')
