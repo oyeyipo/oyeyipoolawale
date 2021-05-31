498c87fee4a1394e8a580a120b623af6d6d5f220
Olawale Oyeyipo <olaclassik@gmail.com>
2021-05-31 14:03:39 +0100

    styling: bio and profile

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStleAndResets />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
@@ -30,9 +32,10 @@ body {
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #999999;
+  color: #777;
   line-height: 1.6;
   font-weight: 400;
+  font-size: 1.6rem;
 }
 
 ::selection {
@@ -55,7 +58,32 @@ a {
   transition: color 0.2s ease, text-decoration-color 0.2s ease;
   text-decoration-thickness: 0.1em;
 }
-
 `;
+
+const theme = {
+  color: {
+    turquoise: "#1abc9c",
+    greenSea: "#16a085",
+    emerald: "#2ecc71",
+    nephritis: "#27ae60",
+    sunFlower: "#f1c40f",
+    orange: "#f39c12",
+    carrot: "#e67e22",
+    pumpkin: "#d35400",
+    alizarin: "#e74c3c",
+    pomegranate: "#c0392b",
+    amethyst: "#9b59b6",
+    wisteria: "#8e44ad",
+    peterRiver: "#3498db",
+    belizeHole: "#2980b9",
+    wetAsphalt: "#34495e",
+    midgniteBlue: "#2c3e50",
+    clouds: "#ecf0f1",
+    silver: "#bdc3c7",
+    concrete: "#95a5a6",
+    asbestos: "#7f8c8d",
+  },
+  fontWeight: [400, 600],
+};
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,19 @@ export default function Home() {
         <title>Oyeyipo Olawale - Software Engineer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <h1>Oyeyipo Olawale</h1>
-        <h2>Software engineer & open sourcerer</h2>
-      </header>
+      <Header>
+        <div>
+          <Avatar>
+            <img />
+          </Avatar>
+          <h1>Oyeyipo Olawale</h1>
+          <h2>Software engineer & open sourcerer</h2>
+          <p>
+            I build and create quality softwares. Mostly for the web and
+            sometimes mobile apps.
+          </p>
+        </div>
+      </Header>
 
       <main>
         <section id="blog-posts">
@@ -37,4 +46,52 @@ const Button = styled.button`
       background-color: palevioletred;
       color: white;
     `}
+`;
+
+const Header = styled.header`
+  height: 40rem;
+  background-color: #2c3e50;
+  clip-path: polygon(0 0, 100% 0, 100% 75%, 0 100%);
+
+  display: grid;
+  place-content: center;
+  justify-items: center;
+
+  div {
+    display: block;
+    text-align: center;
+    width: 45rem;
+    z-index: 1;
+  }
+
+  h1 {
+    color: ${(props) => props.theme.color.clouds};
+    font-size: 3.5rem;
+    letter-spacing: 0.2rem;
+  }
+
+  h2 {
+    color: ${(props) => props.theme.color.turquoise};
+    font-size: 2rem;
+  }
+
+  p {
+    color: ${(props) => props.theme.color.silver};
+    margin-top: 0.5rem;
+    font-weight: ${(props) => props.theme.fontWeight[1]};
+  }
+`;
+
+const Avatar = styled.div`
+  border-top: 3px solid ${(props) => props.theme.color.turquoise};
+
+  img {
+    height: 14rem;
+    width: 14rem;
+    background-color: ${(props) => props.theme.color.wetAsphalt};
+    background-size: cover;
+    border-radius: 100%;
+    border: 3px solid ${(props) => props.theme.color.turquoise};
+    margin-top: -7rem;
+  }
 `;
